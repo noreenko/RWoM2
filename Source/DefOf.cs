ab186c2e0515b1b9ff96b01c2c5e6ece0f3bc394
Kolton Noreen <noreenko@users.noreply.github.com>
2023-04-11 22:47:03 -0500

    Figuring out how to do Fireball

--- Source/DefOf.cs
+++ Source/DefOf.cs
@@ -1,9 +1,14 @@
-﻿using VFECore.Abilities;
+﻿using System.Diagnostics.CodeAnalysis;
+using Verse;
+using VFECore.Abilities;
 
 namespace RimWorldOfMagic;
 
 [RimWorld.DefOf]
+[SuppressMessage("ReSharper", "UnassignedField.Global")]
 public static class DefOf
 {
-    public static AbilityDef Fireball;
+    public static AbilityDef RWoM_Ability_Fireball;
+
+    public static SoundDef RWoM_SoftExplosion;
 }
---
+++ Source/ThingDefs/Projectiles/Fireball.cs
@@ -0,0 +1,24 @@
+﻿using Verse;
+
+namespace RimWorldOfMagic.ThingDefs.Projectiles;
+
+public class Fireball : Projectile_Explosive
+{
+    // TODO: Make fireball correctly determine damage from Ability (Should this be done in Ability when spawning projectile?)
+    //      And how many explosions should happen. This function needs some serious refactoring. Unsure if we need a new base class instead of Projectile
+    // protected override void Impact(Thing hitThing, bool blockedByShield = false)
+    // {
+    //     base.Impact(hitThing, blockedByShield);
+    //     int damageAmount = def.projectile.GetDamageAmount(1);
+    //     GenExplosion.DoExplosion(
+    //         Position, Map, def.projectile.explosionRadius, DamageDefOf.Bomb, launcher,
+    //         Mathf.RoundToInt(Rand.Range(damageAmount/2, damageAmount)),
+    //         0, DefOf.RWoM_SoftExplosion, equipmentDef, def,
+    //         chanceToStartFire: 0.1f, damageFalloff: true
+    //     );
+    //
+    //     CellRect cellRect = CellRect.CenteredOn(Position, 5);
+    //     cellRect.ClipInsideMap(Map);
+    //
+    // }
+}
